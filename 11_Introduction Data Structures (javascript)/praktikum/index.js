1d391e8c6dbe114b9cbf48dc9bb8adcbcdd9344b
agusbw <agusbudhiarta81@gmail.com>
2023-02-28 21:44:06 +0800

    chore: add introduction data structure task

--- 11_Introduction Data Structures (javascript)/praktikum/index.js
+++ 11_Introduction Data Structures (javascript)/praktikum/index.js
@@ -1,5 +1,6 @@
 addEventListener("DOMContentLoaded", () => {
   //declare semua element input
+  const form = document.getElementById("form");
   const submitButton = document.getElementById("submitBtn");
   const productName = document.getElementById("productName");
   const productPrice = document.getElementById("productPrice");
@@ -7,6 +8,14 @@ addEventListener("DOMContentLoaded", () => {
   const productCategory = document.getElementById("productCategory");
   const radios = document.querySelectorAll('input[type="radio"]');
   const productImage = document.getElementById("productImage");
+  const inputs = [
+    productName,
+    productPrice,
+    productDescription,
+    productCategory,
+    productImage,
+    ...radios,
+  ];
 
   // declare semua element feedback (element untuk menampilkan pesan error)
   const productNameFeedback = document.getElementById("productNameFeedback");
@@ -17,6 +26,7 @@ addEventListener("DOMContentLoaded", () => {
   const productCategoryFeedback = document.getElementById(
     "productCategoryFeedback"
   );
+
   const productImageFeedback = document.getElementById("productImageFeedback");
   const productFreshnessFeedback = document.getElementById(
     "productFreshnessFeedback"
@@ -26,24 +36,19 @@ addEventListener("DOMContentLoaded", () => {
   const dataAlert = document.querySelector("#dataAlert");
   dataAlert.style.display = "none";
 
-  let products = [];
-
   // Validasi input kosong, akan mengirim object dengan message dan isValid
   const validateIsEmpty = (fieldName, node) => {
     if (node.value == "") {
       return {
         message: `The ${fieldName} field must be filled in`,
-        isEmpty: false,
-      };
-    } else {
-      return {
-        message: "",
         isEmpty: true,
       };
     }
+    return false;
   };
 
-  const validateForm = () => {
+  // Validasi form apakah sudah terisi semua
+  const validateAllInputFilled = () => {
     const validatedName = validateIsEmpty("product name", productName);
     const validatedPrice = validateIsEmpty("product price", productPrice);
     const validatedDescription = validateIsEmpty(
@@ -56,31 +61,30 @@ addEventListener("DOMContentLoaded", () => {
     );
     const validatedImage = validateIsEmpty("product image", productImage);
 
-    // const isRadioChecked = radio1.checked || radio2.checked || radio3.checked;
-    var radiosArr = Array.from(radios);
+    const radiosArr = Array.from(radios);
     const isRadioChecked = radiosArr.some((radio) => radio.checked);
 
-    if (!validatedName.isEmpty) {
+    if (validatedName.isEmpty) {
       productName.classList.add("is-invalid");
       productNameFeedback.textContent = validatedName.message;
     }
 
-    if (!validatedPrice.isEmpty) {
+    if (validatedPrice.isEmpty) {
       productPrice.classList.add("is-invalid");
       productPriceFeedback.textContent = validatedPrice.message;
     }
 
-    if (!validatedDescription.isEmpty) {
+    if (validatedDescription.isEmpty) {
       productDescription.classList.add("is-invalid");
       productDescriptionFeedback.textContent = validatedDescription.message;
     }
 
-    if (!validatedCategory.isEmpty) {
+    if (validatedCategory.isEmpty) {
       productCategory.classList.add("is-invalid");
       productCategoryFeedback.textContent = validatedCategory.message;
     }
 
-    if (!validatedImage.isEmpty) {
+    if (validatedImage.isEmpty) {
       productImage.classList.add("is-invalid");
       productImageFeedback.textContent = validatedImage.message;
     }
@@ -92,17 +96,18 @@ addEventListener("DOMContentLoaded", () => {
       productFreshnessFeedback.textContent = "Please select one of the options";
     }
 
+    // return true jika semua input sudah valid
     return (
-      validatedName.isEmpty &&
-      validatedPrice.isEmpty &&
-      validatedDescription.isEmpty &&
-      validatedCategory.isEmpty &&
-      validatedImage.isEmpty &&
+      !validatedName.isEmpty &&
+      !validatedPrice.isEmpty &&
+      !validatedDescription.isEmpty &&
+      !validatedCategory.isEmpty &&
+      !validatedImage.isEmpty &&
       isRadioChecked
     );
   };
 
-  // Cek apakah form sudah valid dengan menghitung jumlah class is-invalid
+  // Cek apakah form sudah valid dengan menghitung jumlah class is-invalid (untuk enable/disable button)
   const checkIsValid = () => {
     const invalids = document.querySelectorAll(".is-invalid");
     if (invalids.length <= 0) {
@@ -111,10 +116,35 @@ addEventListener("DOMContentLoaded", () => {
     return false;
   };
 
+  function rederTable(products) {
+    const tableBody = document.querySelector("#tableBody");
+    tableBody.innerHTML = "";
+    products.forEach((product, index) => {
+      tableBody.innerHTML += `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${product.name}</td>
+          <td>${product.category}</td>
+          <td>${product.image}</td>
+          <td>${product.freshness}</td>
+          <td>${product.description}</td>
+          <td>${product.price}</td>
+          <td>
+              <button class="btn btn-danger btn-sm">
+                Delete
+              </button>
+          </td>
+        </tr>
+        `;
+    });
+  }
+
+  //declare array untuk menampung data
+  const products = [];
   //Submit form
-  submitButton.addEventListener("click", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (validateForm()) {
+    if (validateAllInputFilled() && checkIsValid) {
       let productFreshness;
       radios.forEach((radio) => {
         radio.checked === true
@@ -124,31 +154,22 @@ addEventListener("DOMContentLoaded", () => {
       const product = {
         name: productName.value,
         category: productCategory.value,
-        image: "default",
+        image: productImage.value,
         freshness: productFreshness,
         description: productDescription.value,
         price: productPrice.value,
       };
-
       products.push(product);
       //render table
-      const tableBody = document.querySelector("#tableBody");
-      tableBody.innerHTML = "";
-      products.forEach((product, index) => {
-        tableBody.innerHTML += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${product.name}</td>
-          <td>${product.category}</td>
-          <td>${product.image}</td>
-          <td>${product.freshness}</td>
-          <td>${product.description}</td>
-          <td>${product.price}</td>
-        </tr>
-        `;
-      });
+      rederTable(products);
       dataAlert.innerHTML = `Data <strong>added</strong> succesfully`;
       dataAlert.style.display = "block";
+
+      //set timeout untuk menghilangkan alert
+      setTimeout(() => {
+        dataAlert.style.display = "none";
+      }, 2000);
+
       // remove all input value
       productName.value = "";
       productPrice.value = "";
@@ -163,82 +184,78 @@ addEventListener("DOMContentLoaded", () => {
     submitButton.disabled = true;
     return false;
   });
-  //Remove error altert ketika input diisi, dan cek apakah form sudah valid (tidak ada simbol dan tidak lebih dari 25 huruf)
-  productName.addEventListener("input", () => {
-    productName.classList.remove("is-invalid");
-    productNameFeedback.textContent = "";
 
-    if (productName.value.length > 25) {
-      productName.classList.add("is-invalid");
-      productNameFeedback.textContent =
-        "The product name must be less than 25 letters";
+  //Remove error altert ketika input diisi
+  inputs.forEach((input) => {
+    if (input.type !== "radio") {
+      input.addEventListener("input", () => {
+        input.classList.remove("is-invalid");
+        input.nextElementSibling.textContent = "";
+        if (input.id === "productName") {
+          if (productName.value.length > 25) {
+            productName.classList.add("is-invalid");
+            productNameFeedback.textContent =
+              "The product name must not exceed 25 characters.";
+          }
+
+          const isNonWordRegex = /[!@#$%^&*(),.?":{}|<>'\\/\-_+=`~\]\[;]/;
+          if (isNonWordRegex.test(productName.value)) {
+            productName.classList.add("is-invalid");
+            productNameFeedback.textContent =
+              "Product name must not contain symbols.";
+          }
+        }
+        checkIsValid()
+          ? (submitButton.disabled = false)
+          : (submitButton.disabled = true);
+      });
+    } else {
+      input.addEventListener("input", () => {
+        radios.forEach((radio, key) => {
+          radio.classList.remove("is-invalid");
+        });
+        radios[2].nextElementSibling.nextElementSibling.textContent = "";
+        checkIsValid()
+          ? (submitButton.disabled = false)
+          : (submitButton.disabled = true);
+      });
     }
+  });
 
-    const isNonWordRegex = /[!@#$%^&*(),.?":{}|<>'\\/\-_+=`~\]\[;]/;
-    if (isNonWordRegex.test(productName.value)) {
-      productName.classList.add("is-invalid");
-      productNameFeedback.textContent =
-        "Product name must not contain symbols.";
+  //Action button
+  //delete data berdasarkan index target (menggunakan event delegation)
+  document.querySelector("#tableBody").addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-danger")) {
+      e.target.parentElement.parentElement.remove();
+      dataAlert.innerHTML = `Data <strong>deleted</strong> succesfully`;
+      dataAlert.style.display = "block";
+      //set timeout untuk menghilangkan alert
+      setTimeout(() => {
+        dataAlert.style.display = "none";
+      }, 2000);
     }
-
-    if (productName.value === "") {
-      productName.classList.remove("is-invalid");
-      productNameFeedback.textContent = "";
-    }
-
-    checkIsValid()
-      ? (submitButton.disabled = false)
-      : (submitButton.disabled = true);
   });
 
-  //Remove error altert ketika input diisi
-  productPrice.addEventListener("input", () => {
-    productPrice.classList.remove("is-invalid");
-    productPriceFeedback.textContent = "";
-
-    checkIsValid()
-      ? (submitButton.disabled = false)
-      : (submitButton.disabled = true);
+  //delete data terakhir yang dimasukkan user
+  document.getElementById("deletion").addEventListener("click", () => {
+    //kurangi array products terakhir
+    products.pop();
+    //re-render table
+    rederTable(products);
+    dataAlert.innerHTML = `Data <strong>deleted</strong> succesfully`;
+    dataAlert.style.display = "block";
+    //set timeout untuk menghilangkan alert
+    setTimeout(() => {
+      dataAlert.style.display = "none";
+    }, 2000);
   });
 
-  //Remove error altert ketika input diisi
-  productDescription.addEventListener("input", () => {
-    productDescription.classList.remove("is-invalid");
-    productDescriptionFeedback.textContent = "";
-
-    checkIsValid()
-      ? (submitButton.disabled = false)
-      : (submitButton.disabled = true);
-  });
-
-  //Remove error altert ketika input diisi
-  productCategory.addEventListener("input", () => {
-    productCategory.classList.remove("is-invalid");
-    productCategoryFeedback.textContent = "";
-
-    checkIsValid()
-      ? (submitButton.disabled = false)
-      : (submitButton.disabled = true);
-  });
-
-  //Remove error altert ketika input diisi
-  productImage.addEventListener("input", () => {
-    productImage.classList.remove("is-invalid");
-    productImageFeedback.textContent = "";
-
-    checkIsValid()
-      ? (submitButton.disabled = false)
-      : (submitButton.disabled = true);
-  });
-
-  //Remove error altert ketika input diisi
-  radios.forEach((radio) => {
-    radio.addEventListener("input", () => {
-      radios.forEach((radio) => radio.classList.remove("is-invalid"));
-      productFreshnessFeedback.textContent = "";
-      checkIsValid()
-        ? (submitButton.disabled = false)
-        : (submitButton.disabled = true);
-    });
+  document.getElementById("search").addEventListener("click", () => {
+    const searchParameter = document.getElementById("searchProduct").value;
+    const searchResult = products.filter((product) =>
+      product.name.includes(searchParameter)
+    );
+    //re-render table
+    rederTable(searchResult);
   });
 });
