dbad8a408e750bfdf7bd5dea8ee57a6edfb4debc
agusbw <agusbudhiarta81@gmail.com>
2023-03-16 10:31:15 +0800

    fix: connecting label to radio button

--- 15_Event Handling/praktikum/create-product-app/src/pages/CreateProduct/ProductForm/ProductForm.jsx
+++ 15_Event Handling/praktikum/create-product-app/src/pages/CreateProduct/ProductForm/ProductForm.jsx
@@ -178,6 +178,7 @@ export default function ProductForm() {
                 value="new"
                 checked={formData.freshness === "new"}
                 onChange={handleChange}
+                id="radio1"
               />
               <label className="form-check-label" htmlFor="radio1">
                 {" "}
@@ -194,6 +195,7 @@ export default function ProductForm() {
                 value="second"
                 checked={formData.freshness === "second"}
                 onChange={handleChange}
+                id="radio2"
               />
               <label className="form-check-label" htmlFor="radio2">
                 Second Hand
@@ -209,6 +211,7 @@ export default function ProductForm() {
                 value="refurbished"
                 checked={formData.freshness === "refurbished"}
                 onChange={handleChange}
+                id="radio3"
               />
               <label className="form-check-label" htmlFor="radio3">
                 Refurbished
