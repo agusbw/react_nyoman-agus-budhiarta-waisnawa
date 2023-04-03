a22fccaf6bab358bbf0106358a1e57a8b0af1f4e
agusbw <agusbudhiarta81@gmial.com>
2023-04-03 21:37:02 +0800

    fix: fix praktikum folder issue

---
+++ 20_Global State Management and Data fetching/praktikum/create-product-app/src/pages/Login/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username === "admin" && password === "admin") {
+      localStorage.setItem("token", "myToken");
+
+      navigate("/");
+    } else {
+      setError("Incorrect username or password");
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <form onSubmit={handleSubmit} className="border rounded p-4">
+        <h2 className="text-center mb-4">Login Page</h2>
+        {error && <p className="text-danger">{error}</p>}
+        <div className="mb-3">
+          <p>
+            username: admin <br />
+            password: admin
+          </p>
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
